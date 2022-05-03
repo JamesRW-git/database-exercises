a5ecc7f897921a3dc645c011dd50d246478c7cf9
James Woodward <jamesrobertwoodward@gmail.com>
2022-05-03 10:19:42 -0500

    complete 2nd part of where clause exercise

--- where_exercises.sql
+++ where_exercises.sql
@@ -2,12 +2,21 @@ use employees;
 
 SELECT *
 from employees
-WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+WHERE first_name = 'Irena'
+OR first_name = 'Vidya'
+OR first_name = 'Maya'
+AND gender = 'Male';
 
 SELECT *
 from employees
-WHERE last_name LIKE 'e%';
+WHERE last_name LIKE 'e%'
+AND last_name like '%e';
 
 SELECT *
 from employees
 WHERE last_name LIKE '%q%';
+
+SELECT *
+from employees
+WHERE last_name LIKE '%q%'
+AND NOT last_name LIKE '%qu%';
